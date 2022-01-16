4306fe0df18bc403c8a1f20befd9c488b9d39a3a
err87 <err87@cornell.edu>
2022-01-15 21:46:23 -0500

    difference of squares

---
+++ kotlin/src/main/kotlin/difference_of_squares/DifferenceOfSquares.kt
@@ -0,0 +1,10 @@
+package difference_of_squares
+
+class Squares(val n: Long) {
+
+    fun sumOfSquares() = (2 * n + 1) * n * (n + 1) / 6
+
+    fun squareOfSum() = (n * (n + 1) / 2).let { it * it }
+
+    fun difference() = squareOfSum() - sumOfSquares()
+}
