c9f5dd2768a576379ba2311b0abc3f7df14ac598
err87 <err87@cornell.edu>
2022-01-15 21:05:40 -0500

    solved

--- kotlin/src/main/kotlin/transpose/Transpose.kt
+++ kotlin/src/main/kotlin/transpose/Transpose.kt
@@ -2,23 +2,13 @@ package transpose
 
 object Transpose {
 
-    fun transpose2(rows: List<String>): List<String> {
-        val result: MutableList<String> = mutableListOf()
-        rows.forEachIndexed { j, col ->
-            col.forEachIndexed { i, c ->
-                if (result.size <= i) result.add(i, "".padEnd(j, ' ') + c)
-                else result[i] = result[i].padEnd(j, ' ') + c
-            }
+    fun transpose(rows: List<String>) =
+        if (rows.isEmpty()) emptyList()
+        else List(rows.maxOf { it.length }) { j ->
+            List(rows.size) { i -> rows[i].getOrElse(j) { '_' } }
+                .dropLastWhile { it == '_' }
+                .joinToString("")
+                .replace('_', ' ')
         }
-        return result
-    }
 
-    fun transpose(rows: List<String>): List<String> {
-        if (rows.isEmpty()) return emptyList()
-        val columns = Array(rows.size) { rows[it].length }
-            .apply { for (i in lastIndex - 1 downTo 0) set(i, maxOf(get(i), get(i + 1))) }
-        return List(columns[0]) { j ->
-            rows.mapIndexedNotNull { i, s -> s.getOrElse(j) { ' ' }.takeIf { j < columns[i] } }.joinToString("")
-        }
-    }
 }
