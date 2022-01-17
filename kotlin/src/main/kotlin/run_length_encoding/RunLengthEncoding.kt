b3d16adeca72f358c6b29ea11657d76cc56522f7
err87 <err87@cornell.edu>
2022-01-17 10:29:03 -0500

    iteration 1

---
+++ kotlin/src/main/kotlin/run_length_encoding/RunLengthEncoding.kt
@@ -0,0 +1,7 @@
+package run_length_encoding
+
+fun encode(input: String) = Regex("(.)\\1*")
+    .findAll(input).joinToString("") { it.groupValues.let { (a, b) -> if (a.length > 1) "${a.length}$b" else b } }
+
+internal fun decode(input: String) = Regex("(\\p{Digit}*)(\\p{Alnum}|\\p{Space})")
+    .findAll(input).joinToString("") { it.destructured.let { (a, b) -> b.repeat(a.toIntOrNull() ?: 1) } }
