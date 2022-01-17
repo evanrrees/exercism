0e8775bda29d14f7186ebf371fdd51a3527d5958
err87 <err87@cornell.edu>
2022-01-16 22:12:34 -0500

    submitted

---
+++ kotlin/src/main/kotlin/saddle_points/Matrix.kt
@@ -0,0 +1,14 @@
+package saddle_points
+
+data class MatrixCoordinate(val row: Int, val col: Int)
+
+data class Matrix(val data: List<List<Int>>) {
+    val saddlePoints: Set<MatrixCoordinate> = mutableSetOf()
+    init {
+        saddlePoints as MutableSet
+        for ((i, row) in data.withIndex())
+            for ((j, int) in row.withIndex())
+                if (row.none { it > int } && data.none { it[j] < int })
+                    saddlePoints += MatrixCoordinate(i + 1, j + 1)
+    }
+}
