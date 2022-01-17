e392267c7287ef36f00d4c29ddbef0f581029a49
err87 <err87@cornell.edu>
2022-01-17 07:45:33 -0500

    submitted

---
+++ kotlin/src/main/kotlin/binary_search/BinarySearch.kt
@@ -0,0 +1,13 @@
+package binary_search
+
+object BinarySearch {
+    tailrec fun search(list: List<Int>, item: Int, left: Int = 0, right: Int = list.lastIndex): Int {
+        val mid: Int = left + ((right - left) shr 1)
+        return when {
+            left > right      -> throw NoSuchElementException()
+            item == list[mid] -> mid
+            item < list[mid]  -> search(list, item, left, mid - 1)
+            else              -> search(list, item, mid + 1, right)
+        }
+    }
+}
