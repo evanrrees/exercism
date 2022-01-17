25b430ba5d8116f1167a6fca1eb81e40af626bf0
err87 <err87@cornell.edu>
2022-01-17 16:18:28 -0500

    wip

--- kotlin/src/main/kotlin/scale_generator/Scale.kt
+++ kotlin/src/main/kotlin/scale_generator/Scale.kt
@@ -1,11 +1,50 @@
 package scale_generator
 
+import rna_transcription.complement
+
+
+enum class Tone {
+
+    A, AB, B, C, CD, D, DE, E, F, FG, G, GA;
+
+    val ambiguous get() = name.length == 2
+    val sharp     get() = if (ambiguous) "${name.first()}#" else null
+    val flat      get() = if (ambiguous) "${name.last()}b" else null
+
+    fun toString(flat: Boolean) = when {
+        !ambiguous -> name
+        flat -> this.flat!!
+        else -> this.sharp!!
+    }
+
+    companion object {
+        fun getValue(string: String): Tone {
+            complement
+            return when (string.last()) {
+                '#' -> values().first { it.sharp == string.take(1) }
+                'b' -> values().first { it.flat == string.take(1) }
+                else -> values().first { it.name == string.take(1) }
+            }
+        }
+    }
+}
+
+data class RealTone(val tone: Tone, val flat: Boolean = false) {
+    override fun toString(): String {
+        return tone.toString(flat)
+    }
+}
 
 class Scale(tonic: String) {
 
     private val _tonic = tonic.first().uppercase() + tonic.drop(1)
 
     private val usesFlats = _tonic.endsWith('b')
+
+    val chromatic2 = Tone.values().run {
+
+        map { RealTone(it, usesFlats) }
+    }
 
     private val chromatic = listOf(
         "A",      if (usesFlats) "Bb" else "A#",
