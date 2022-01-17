9e04a11d26c8a68de7751c27dc5564f97fb54802
err87 <err87@cornell.edu>
2022-01-17 17:56:51 -0500

    iteration 2

--- kotlin/src/main/kotlin/perfect_numbers/NaturalNumber.kt
+++ kotlin/src/main/kotlin/perfect_numbers/NaturalNumber.kt
@@ -5,16 +5,11 @@ enum class Classification {
     DEFICIENT, PERFECT, ABUNDANT
 }
 
-fun classify(naturalNumber: Int): Classification {
-    if (naturalNumber < 1) throw RuntimeException("$naturalNumber is not a natural number.")
-    val aSum = aliquotSum(naturalNumber)
-    return when {
-        aSum == naturalNumber -> Classification.PERFECT
-        aSum > naturalNumber -> Classification.ABUNDANT
+tailrec fun classify(number: Int, factor: Int = 1, sum: Int = 0): Classification =
+    when {
+        number < 1 -> throw RuntimeException("$number is not a natural number.")
+        factor <= number / 2 -> classify(number, factor + 1, sum + if (number % factor == 0) factor else 0)
+        sum == number -> Classification.PERFECT
+        sum > number -> Classification.ABUNDANT
         else -> Classification.DEFICIENT
     }
-}
-
-tailrec fun aliquotSum(number: Int, factor: Int = 1, sum: Int = 0): Int =
-    if (factor > number / 2) sum
-    else aliquotSum(number, factor + 1, sum + if (number % factor == 0) factor else 0)
