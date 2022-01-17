d60c06a63315fa1c78742f6d548dfd652d877543
err87 <err87@cornell.edu>
2022-01-17 01:09:06 -0500

    annotations I'd never heard of

--- kotlin/src/main/kotlin/bank_account/BankAccount.kt
+++ kotlin/src/main/kotlin/bank_account/BankAccount.kt
@@ -1,26 +1,22 @@
 package bank_account
 
-import java.util.concurrent.BlockingQueue
-import java.util.concurrent.LinkedBlockingQueue
 
 class BankAccount {
 
     private var open = true
-    val balance get() = _balance
-    private var _balance = 0L
+
+    @Volatile
+    var balance = 0L
         get() {
             check(open) { "Account is closed." }
-            while (transactionQueue.isNotEmpty()) {
-                field += transactionQueue.take()
-            }
             return field
         }
+        private set
 
-    private val transactionQueue: BlockingQueue<Long> = LinkedBlockingQueue()
-
+    @Synchronized
     fun adjustBalance(amount: Long) {
         check(open) { "Account is closed." }
-        transactionQueue += amount
+        balance += amount
     }
 
     fun close() {
