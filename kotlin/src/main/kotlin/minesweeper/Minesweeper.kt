0841c3e5496a0625cb28087bf64018bdfba69681
err87 <err87@cornell.edu>
2022-01-18 14:13:21 -0500

    iteration 1

---
+++ kotlin/src/main/kotlin/minesweeper/Minesweeper.kt
@@ -0,0 +1,23 @@
+package minesweeper
+
+data class MinesweeperBoard(val board: List<String>) {
+
+    val offsets = listOf(-1, 0, 1)
+    fun withNumbers(): List<String> {
+        return board.mapIndexed { i, row ->
+            row.withIndex().joinToString("") { (j, c) ->
+                if (c == '*') "$c"
+                else {
+                    val n = offsets.sumOf { i2: Int ->
+                        offsets.count { j2: Int ->
+                            (i2 != 0 || j2 != 0) && board.getOrNull(i + i2)?.getOrNull(j + j2) == '*'
+                        }
+                    }
+                    if (n > 0) "$n"
+                    else "$c"
+                }
+            }
+        }
+    }
+
+}
