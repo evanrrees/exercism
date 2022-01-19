86e6e70e048215304049958c28593a2e697e190a
err87 <err87@cornell.edu>
2022-01-19 11:36:30 -0500

    iteration 3 - brute force but passing

--- kotlin/src/main/kotlin/sum_of_multiples/SumOfMultiples.kt
+++ kotlin/src/main/kotlin/sum_of_multiples/SumOfMultiples.kt
@@ -2,25 +2,13 @@ package sum_of_multiples
 
 object SumOfMultiples {
 
-    tailrec fun gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
-    fun lcm(a: Int, b: Int) = a * b / gcd(a, b)
-
-    tailrec fun sum(factors: Set<Int>, limit: Int, sum: Int = 0, coef: Int = 1): Int {
-        val realFactors = factors.filter { it in 1..limit }
-        val newFactors = mutableSetOf<Int>()
-        var total = 0
-        for ((i, factor) in realFactors.withIndex()) {
-            val n = (limit - 1) / factor
-            total += factor * n * (n + 1) / 2
-            for (j in i + 1 .. realFactors.lastIndex) {
-                if (j > realFactors.lastIndex) continue
-                newFactors += lcm(factor, realFactors[j])
-            }
+    fun sum(factors: Set<Int>, limit: Int): Int {
+        if (factors.isEmpty()) return 0
+        val include = Array(limit) { false }
+        for (f in factors) if (f in 1..limit) for (i in f until limit step f) {
+            include[i] = true
         }
-        return if (newFactors.isEmpty())
-            sum + coef * total
-        else
-            sum(newFactors.toSortedSet(), limit, sum + coef * total, coef * -1)
+        return include.withIndex().sumOf { if (it.value) it.index else 0 }
     }
 
 }
