f8d14c1ed34ace0036b39cf2612040926b3f9706
err87 <err87@cornell.edu>
2022-01-20 20:04:34 -0500

    iteration 2 with bitshifts

--- kotlin/src/main/kotlin/grains/Grains.kt
+++ kotlin/src/main/kotlin/grains/Grains.kt
@@ -1,22 +1,12 @@
 package grains
 
 import java.math.BigInteger
-import java.math.BigInteger.*
 
 internal object Board {
 
-    private val grains = Array<BigInteger>(64) { ONE }
-
     fun getGrainCountForSquare(number: Int): BigInteger {
-        return if (number == 1)
-            grains[0]
-        else if (grains[number - 1] != ONE)
-            grains[number - 1]
-        else {
-            val count = getGrainCountForSquare(number - 1) shl 1
-            grains[number - 1] = count
-            count
-        }
+        require(number in 1..64)
+        return BigInteger.ONE shl (number - 1)
     }
 
     fun getTotalGrainCount() = getGrainCountForSquare(64).shl(1).dec()
