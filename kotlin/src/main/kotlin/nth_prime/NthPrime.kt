be319e31df5238818699754b9ed2ce13c4d559c7
err87 <err87@cornell.edu>
2022-01-21 00:23:56 -0500

    iteration 2 - fixed bug in init no crazy object

--- kotlin/src/main/kotlin/nth_prime/NthPrime.kt
+++ kotlin/src/main/kotlin/nth_prime/NthPrime.kt
@@ -2,16 +2,17 @@ package nth_prime
 
 internal object Prime {
 
-    private const val SIEVE_SIZE = 46_300
-
+    private const val OVERFLOW_I = 46_341
+    private const val SIEVE_SIZE = 125_000
     private val sieve = Array(SIEVE_SIZE) { it % 2 == 1 }
 
     init {
         sieve[1] = false
         sieve[2] = true
-        for (i in 3..sieve.lastIndex step 2) if (sieve[i])
-            for (j in i * i .. sieve.lastIndex step i)
-                sieve[j] = false
+        for (i in 3 until SIEVE_SIZE step 2)
+            if (sieve[i] && i < OVERFLOW_I)
+                for (j in i * i until SIEVE_SIZE step i)
+                    sieve[j] = false
     }
 
     private val primes = sieve.withIndex().mapNotNull { if (it.value) it.index else null }
