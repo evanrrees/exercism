9a6650d363bcf7005e7054c11e74abfb1fc58452
err87 <err87@cornell.edu>
2022-01-22 17:52:46 -0500

    single line

--- kotlin/src/main/kotlin/matching_brackets/MatchingBrackets.kt
+++ kotlin/src/main/kotlin/matching_brackets/MatchingBrackets.kt
@@ -2,9 +2,8 @@ package matching_brackets
 
 object MatchingBrackets {
 
-    fun isValid(string: String): Boolean {
-        val match = regex.find(string) ?: return string.none { it in brackets }
-        return isValid(string.replaceRange(match.range, ""))
+    tailrec fun isValid(string: String): Boolean {
+        return isValid(string.replace(regex, "").takeIf { it != string } ?: return string.none { it in brackets })
     }
 
     private val regex = Regex("\\[[^{(\\[)}\\]]*]|\\{[^{(\\[)}\\]]*}|\\([^{(\\[)}\\]]*\\)")
