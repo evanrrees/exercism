478883d5d394464ce992ef9bbd28fb99168f838f
err87 <err87@cornell.edu>
2022-01-23 11:08:37 -0500

    iteration 1

--- kotlin/src/main/kotlin/change/ChangeCalculator.kt
+++ kotlin/src/main/kotlin/change/ChangeCalculator.kt
@@ -1,22 +1,19 @@
 package change
 
-internal class ChangeCalculator(coins: List<Int>) {
-    private val coins = coins.sortedDescending()
+internal class ChangeCalculator(private val coins: List<Int>) {
 
     fun computeMostEfficientChange(grandTotal: Int): List<Int> {
         require(grandTotal >= 0) { "Negative totals are not allowed." }
 
         var bestPath = emptyList<Int>()
-        fun dfs(total: Int, _coins: List<Int>, path: List<Int> = emptyList()) {
+        fun dfs(total: Int, _coins: List<Int> = coins.sortedDescending(), path: List<Int> = emptyList()) {
             if (bestPath.isEmpty() || path.size < bestPath.size)
                 for ((index, c) in _coins.withIndex())
-                    if (c == total && (bestPath.isEmpty() || path.size + 1 < bestPath.size))
-                        bestPath = path + c
-                    else if (c < total)
-                        dfs(total - c, _coins.drop(index), path + c)
+                    if (c == total) bestPath = path + c
+                    else if (c < total) dfs(total - c, _coins.drop(index), path + c)
         }
 
-        dfs(grandTotal, coins)
+        dfs(grandTotal)
         require(grandTotal == 0 || bestPath.isNotEmpty()) { "The total $grandTotal cannot be represented in the given currency." }
         return bestPath
     }
