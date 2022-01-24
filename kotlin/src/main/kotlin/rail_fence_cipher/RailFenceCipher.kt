28454de2272594fc94c58ea88b441700734cad49
err87 <err87@cornell.edu>
2022-01-23 22:06:09 -0500

    iteration 2

--- kotlin/src/main/kotlin/rail_fence_cipher/RailFenceCipher.kt
+++ kotlin/src/main/kotlin/rail_fence_cipher/RailFenceCipher.kt
@@ -4,27 +4,18 @@ internal class RailFenceCipher(private val rails: Int) {
 
     private val stride = 2 * (rails - 1)
 
-    fun getEncryptedData(input: String) = buildString(input.length) {
-        for (rail in 0 until rails) {
-            for (pos in input.indices step stride) {
-                input.getOrNull(pos + rail)?.let(::append)
-                if (rail in 1 until rails - 1)
-                    input.getOrNull(pos + stride - rail)?.let(::append)
-            }
-        }
-    }
+    fun getEncryptedData(input: String) = encryptionSequence(input.length).map(input::get).joinToString("")
 
-    fun getDecryptedData(input: String): String {
-        val result = Array(input.length) { '.' }
-        val iterator = input.iterator()
-        for (rail in 0 until rails) {
-            for (pos in input.indices step stride) {
-                if (pos + rail in input.indices) result[pos + rail] = iterator.next()
-                if (rail in 1 until rails - 1)
-                    if (pos + stride - rail in input.indices)
-                        result[pos + stride - rail] = iterator.next()
+    fun getDecryptedData(input: String) = decryptionSequence(input.length).map(input::get).joinToString("")
+
+    private fun encryptionSequence(length: Int) = sequence {
+        for (rail in 0 until rails)
+            for (pos in rail until length step stride) {
+                yield(pos)
+                if (rail > 0 && rail < rails - 1) yield(pos + stride - 2 * rail)
             }
-        }
-        return result.joinToString("")
-    }
+    }.filter { it < length }
+
+    private fun decryptionSequence(length: Int) =
+        encryptionSequence(length).withIndex().sortedBy { it.value }.map { it.index }
 }
