584e00144028293791dff31a50f08484609ac10e
err87 <err87@cornell.edu>
2022-01-25 15:50:37 -0500

    iteration 2

--- kotlin/src/main/kotlin/diffie_hellman/DiffieHellman.kt
+++ kotlin/src/main/kotlin/diffie_hellman/DiffieHellman.kt
@@ -7,7 +7,7 @@ internal object DiffieHellman {
 
     fun privateKey(p: BigInteger): BigInteger = BigInteger.valueOf(Random.nextLong(2, p.longValueExact()))
 
-    fun publicKey(p: BigInteger, g: BigInteger, prv: BigInteger): BigInteger = g.pow(prv.intValueExact()).mod(p)
+    fun publicKey(p: BigInteger, g: BigInteger, prv: BigInteger): BigInteger = g.modPow(prv, p)
 
-    fun secret(p: BigInteger, pub: BigInteger, prv: BigInteger): BigInteger = pub.pow(prv.intValueExact()).mod(p)
+    fun secret(p: BigInteger, pub: BigInteger, prv: BigInteger): BigInteger = pub.modPow(prv, p)
 }
