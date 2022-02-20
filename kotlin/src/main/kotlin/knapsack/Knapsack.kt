26bac52fd6851ee375492b65e19acc69af8b0837
evanrr <evanrr@protonmail.com>
2022-02-20 15:12:23 -0500

    iteration 3 - faster recursion w/ seen array

--- kotlin/src/main/kotlin/knapsack/Knapsack.kt
+++ kotlin/src/main/kotlin/knapsack/Knapsack.kt
@@ -12,24 +12,19 @@ operator fun Item.plus(other: Item) = Item(weight + other.weight, value + other.
 fun knapsack(maximumWeight: Int, items: List<Item>): Int {
     if (items.isEmpty()) return 0
     var best = Item(0, 0)
-
-    fun helper2(knapsack: Item = Item(0, 0), others: List<Item> = items) {
-        var foundOther = false
-        for (item in others) {
-            if (item.weight + knapsack.weight <= maximumWeight) {
-                foundOther = true
-                helper2(knapsack + item, others - item)
+    val seen = BooleanArray(items.size)
+    fun helper2(knapsack: Item = Item(0, 0)) {
+        var checkBest = true
+        for (index in items.indices) {
+            if (!seen[index] && items[index].weight + knapsack.weight <= maximumWeight) {
+                checkBest = false
+                seen[index] = true
+                helper2(knapsack + items[index])
+                seen[index] = false
             }
         }
-        if (!foundOther && knapsack.value > best.value) best = knapsack
-    }
-
-    fun helper(knapsack: Item = Item(0, 0), others: List<Item> = items) {
-        val candidates = others.filter { it.weight + knapsack.weight <= maximumWeight }
-        if (candidates.isEmpty()) {
-            if (knapsack.value > best.value) best = knapsack
-        } else
-            for (other in candidates) helper(knapsack + other, others - other)
+        if (checkBest && knapsack.value > best.value)
+            best = knapsack
     }
     helper2()
     return  best.value
